d641b9076b8a0bce9d8f887a42aa6dc09c32ecea
Ammar Shaar <84602460+AmmarShaar@users.noreply.github.com>
2021-07-03 14:28:19 +0300

    Add files via upload

---
+++ FE/app.js
@@ -0,0 +1,232 @@
+let thePokemons = [];
+const pokedex = document.getElementById('pokedex');
+const searchtext = document.getElementById('search-txt');
+const fetchPokemon = () => {
+                                            // for (let i =1; i<= 150; i++) {
+                                            //     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+                                            //     fetch(url)
+                                            //         .then((res) => {
+                                            //             return res.json();
+                                            //         })
+                                            //         .then((data) => {
+                                            //             console.log(data);
+                                            //             const pokemon ={
+                                            //                 name: data.name,
+                                            //                 id: data.id,
+                                            //                 image: data.sprites['front_default'],
+                                            //                 type: data.types.map((type) => type.type.name).join(", ")
+                                            //             };
+
+                                            //             console.log(pokemon);
+                                            //         });
+                                            // }
+    const promises = [];
+
+    const usedIndexes = new Set();    
+    function getUniqueRandomNumber(max, min = 1) {
+        const newNumber = Math.floor(Math.random() * (max - min) + min);
+        if (usedIndexes.has(newNumber)) {
+            return this.getUniqueRandomNumber(max, min);
+        } else { 
+            usedIndexes.add(newNumber);
+            return newNumber;
+        }
+    }
+    
+    for (let i=1; i<= 5; i++) {
+        const index = getUniqueRandomNumber(898,1);
+        const url = `http://localhost:5000/pokemon/${index}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data[0].name.english,
+            id: data[0].id,
+            image : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data[0].id}.png`,
+            type: data[0].type.join(", "),
+            hp: data[0].base["HP"],
+            attack: data[0].base["Attack"],
+            defense: data[0].base["Defense"],
+            special_attack: data[0].base["Sp. Attack"],
+            special_defense: data[0].base["Sp. Defense"],
+            speed: data[0].base["Speed"],
+            //moves: data[0].moves,//.map((m)=> m.move.name),
+            captured: (localStorage.getItem(data.id) == "true")? "true": "false"
+            
+        }));
+        thePokemons = pokemon;
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon.moves);
+    const pokemonHTMLString = pokemon
+      .map(
+        (pokeman) => 
+        `
+    <li class="card" >
+        <p class="card-id"> ${pokeman.id} </p>
+        <img class="card-image" src="${pokeman.image}" onclick="selectPokemon(${pokeman.id})">
+        <h2 class="card-title">${pokeman.name}</h2>
+        <p class="card-subtitle"> ${pokeman.type} </p>
+        
+    <label class="switch">
+    <input type="checkbox" onclick="switcherFunction(${pokeman.id})" id="${pokeman.id}">
+    <span class="slider round"></span>
+</label>
+    </li>
+    `
+      )
+      .join("");
+    pokedex.innerHTML = pokemonHTMLString;
+    pokemon.map((pokeman)=> getCaptureValuesFromStorage(pokeman.id));
+};
+
+function getCaptureValuesFromStorage(id){
+    if (localStorage.getItem(id) == "true") { 
+        document.getElementById(id).checked = true;
+    }else{
+        document.getElementById(id).checked = false;
+    }
+}
+
+function switcherFunction(id){
+    const url = `http://localhost:5000/pokemon/${id}`;
+    if(document.getElementById(id).checked){
+        localStorage.setItem(id, "true");//
+        fetch(url, {
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            method: 'PUT',
+            body: JSON.stringify({
+             captured:true,
+            })
+          }).catch(function(error) {                        // catch
+            console.log('Request failed', error);
+          });
+        // fetch(url, {
+        //     headers: { 'Content-Type': 'application/json' }, // tells the server we have json
+        //     method:'PUT', // can be POST
+        //     body: JSON.stringify({
+        //         id: id,
+        //         captured:true,
+        //     }), // json is sent to the server as text
+        //   })
+        // const request = new Request(url, {
+        //     headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        //     method :'PUT',
+        //     body: JSON.stringify({
+        //         captured:true,
+        //     })
+        // })
+        // fetch(request).then((res)=>{
+        //     if (res.status === 500) {
+        //         return res.json() // return the result of the inner promise, which is an error
+        //         .then((json) => {
+        //           const { message, stackTrace } = json;
+        //           throw new ServerException(message, stackTrace);
+        //         });
+        //       } else {
+        //         return res.json();
+        //       }
+        // })
+        
+    }
+    else{
+        localStorage.setItem(id, "false");//
+        fetch(url, {
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            method: 'PUT',
+            body: JSON.stringify({
+              captured:false,
+            })
+          })
+   }
+}
+
+let filteredPokemons;
+searchtext.addEventListener("keyup", (e) => {
+    const searchString = e.target.value.toLowerCase();
+    filteredPokemons = thePokemons.filter((p)=>{
+        return (p.name.includes(searchString));
+    });
+    displayPokemon(filteredPokemons);
+
+
+});
+
+const selectPokemon = async (id) => {
+    for (var i=0; i < thePokemons.length; i++) {
+        if (thePokemons[i].id === id) {
+            //console.log(id);
+            displayPopup(thePokemons[i]);
+            //return myArray[i];
+        }
+    }
+};
+
+let displayedPokemons;
+const displayPopup = (pokeman) => {
+    
+    displayedPokemons = pokedex.innerHTML;
+    let htmlString = `
+        <div class="popup">
+            <button id="closeBtn" onclick="closePopup()">X</button>
+            <div class="card">
+            <img class="card-imagepop" src="${pokeman.image}">
+            <h2 class="card-title">${pokeman.name}</h2>
+            <hr>
+            <h3> Type(s) </h3>
+            <p class="card-subtitle"> ${pokeman.type} </p>
+            <hr>
+            <h3> Stats </h3>
+            
+            <p> HP: </p> <div id="theBar">
+                         <div id="bar" style="width: ${pokeman.hp > 100? '100': pokeman.hp}%"><br></div>
+                         <div id="textbar"> ${pokeman.hp}%</div>
+                        </div>
+
+            <p> Attack: </p> <div id="theBar">
+                                                <div id="bar" style="width: ${pokeman.attack > 100? '100': pokeman.attack}%"><br></div>
+                                                <div id="textbar"> ${pokeman.attack}%</div>
+                                                </div>
+            <p> Defense: </p> 
+                                                <div id="theBar">
+                                                <div id="bar" style="width: ${pokeman.defense > 100? '100': pokeman.defense}%"><br></div>
+                                                <div id="textbar"> ${pokeman.defense}%</div>
+                                                </div>
+            <p> Special Attack:</p>
+                                                <div id="theBar">
+                                                <div id="bar" style="width: ${pokeman.special_attack > 100? '100': pokeman.special_attack}%"><br></div>
+                                                <div id="textbar"> ${pokeman.special_attack}%</div>
+                                                </div>
+            <p> Special Defense:</p>
+                                                <div id="theBar">
+                                                <div id="bar" style="width: ${pokeman.special_defense > 100? '100': pokeman.special_defense}%"><br></div>
+                                                <div id="textbar"> ${pokeman.special_defense}%</div>
+                                                </div>
+            <p> Speed:</p>
+                                                <div id="theBar">
+                                                <div id="bar" style="width: ${pokeman.speed > 100? '100': pokeman.speed}%"><br></div>
+                                                <div id="textbar"> ${pokeman.speed}%</div>
+                                                </div>
+            <br><hr>
+            <!--<h3> Moves</h3>-->
+            <!--<ul class="moves">-->
+            
+    `;
+    // pokeman.moves.forEach((p)=>{
+    //     //console.log(p);
+    //     htmlString=htmlString + '<li class="move">'+p+'</li>';
+    // })
+    pokedex.innerHTML = htmlString + '</div></div>';
+};
+
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+    pokedex.innerHTML = displayedPokemons;
+};
+
+fetchPokemon();
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+
+app.use(express.json()); 
+
+//Serve frontend
+//app.use(express.static(path.join(__dirname, 'FE')));
+app.use(express.static(path.join(__dirname,'FE')));
+
+
+//Get pokemons
+var pokemons = JSON.parse(fs.readFileSync('pokedexcopy.json'));
+app.get('/allpokemons', (req,res)=> {
+    res.json(pokemons);
+});
+
+//Get 5 random Pokemons
+// app.get('/five', (req,res)=> {
+//     let fivePokemons = pokemons.slice(0,5);
+//     res.json(fivePokemons);
+// });
+
+//Get Pokemon details
+app.get('/pokemon/:id', (req,res)=> {
+    //res.status(200).json({msg: `${pokemons[0].moves}` })
+    var id = req.params.id;
+    res.json(pokemons.filter(p => p.id === parseInt(id) ));
+});
+
+//Update Pokemon based on capture status
+app.put('/pokemon/:id',(req , res)=>{
+    console.log(req.params.id);//
+    const found = pokemons.some(pokemon => pokemon.id === parseInt(req.params.id));
+    if(found){
+        const updatedPokemon = req.body ;
+        res.json({msg:"Pokemon updated successfully!!!" , pokemon});
+        
+        pokemons.forEach(pokemon =>{
+            if(pokemon.id === parseInt(req.params.id)){
+                pokemon.captured = updatedPokemon.captured ;
+                res.json({msg:"Pokemon updated successfully!" , pokemon});
+            }
+        })
+        fs.writeFile('pokedexcopy.json',JSON.stringify(pokemons,null,2),
+        (err)=>{
+            console.log(err);
+        });
+    }else{
+        res.status(400);
+    }
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=> console.log('Server started'));
